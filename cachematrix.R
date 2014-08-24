1fa8eb9c25dddbb1a9f21c06ecf4de4aa65fe5cd
Mrinmoyee <mrinmoyee.funfreak@gmail.com>
2014-08-24 18:40:25 +0530

    cachematrix.R
    programming assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function "makeCacheMatrix() uses a matrix (that is assumed always
+## square and invertible and stores it in the cache, and then the second
+## cacheSolve() function solves it for it's inverse and stores that value.
+## if the inverse has already been computed it simply finds that value in the 
+## cached data and reproduces it
 
-## Write a short comment describing this function
+## FUNCTION 1: to store matrix in cached data
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { ##input is a matrix (square, invertible)
+        s <- NULL                           ## stored inverted matrix (from cacheSolve()
+                                            ## is reset to NULL everytime a new matrix is 
+                                            ## stored in makeCachematrix()
+        	set <- function(y) {
+                x <<- y
+                s <<- NULL
+        	}
 
+        get <- function() x
+
+       	 setinverse <- function(solve) s <<- solve
+
+        getinverse <- function() s
+
+        list(set = set, get = get,          ## this is the list returned when makeCacheMatrix()
+             setinverse = setinverse,       ## is called, but these are only used in cacheSolve()
+             getinverse = getinverse)
 }
 
+## FUNCTION 2: to get matrix inverse or calculate it
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getinverse()                    ## function caluculates inverse of x from makeCacheMatrix()
+        if(!is.null(s)) {                      ## unless it's already calculated (i.e. not NULL), in which
+                message("getting cached data") ## case, it simply fetches it from cache
+                return(s)
+        }
+        data <- x$get()                        ## reaches this if it is not already calculated
+        s <- solve(data, ...)
+        x$setinverse(s)
+        s
 }
